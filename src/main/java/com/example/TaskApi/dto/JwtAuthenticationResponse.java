9432a23b31af7b58e854ce9dc045378b99c3e694
M-Sayed939 <104098022+M-Sayed939@users.noreply.github.com>
2025-10-23 10:46:34 +0300

    Creating the DTOs to define the JSON structure for the API requests and responses.

---
+++ src/main/java/com/example/TaskApi/dto/JwtAuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.TaskApi.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class JwtAuthenticationResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
+    public JwtAuthenticationResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
