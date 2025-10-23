9c29a67ddaec51c9868a616dc8063911336de6d8
M-Sayed939 <104098022+M-Sayed939@users.noreply.github.com>
2025-10-23 10:54:53 +0300

    Creating the AuthenticationService to handle the business logic (checking for duplicate emails, creating new users, verifying passwords).

---
+++ src/main/java/com/example/TaskApi/services/AuthenticationService.java
@@ -0,0 +1,46 @@
+package com.example.TaskApi.services;
+
+import com.example.TaskApi.dto.LoginRequest;
+import com.example.TaskApi.dto.RegisterRequest;
+import com.example.TaskApi.model.Role;
+import com.example.TaskApi.model.User;
+import com.example.TaskApi.repository.UserRepository;
+import com.example.TaskApi.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    public User register(RegisterRequest registerRequest) {
+        if (userRepository.existsByEmail(registerRequest.getEmail())) {
+            throw new RuntimeException("Email is already in use");
+        }
+        User user = User.builder().
+                name(registerRequest.getName())
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(Role.ROLE_USER)
+                .build();
+        return userRepository.save(user);
+    }
+
+    public String login(LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
+        );
+        return jwtTokenProvider.generateToken(authentication);
+    }
+}
